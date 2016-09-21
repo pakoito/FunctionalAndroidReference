c13d4004ad03c60e9eecfdc344a1353513a99727
pakoito <jueces.jus@gmail.com>
2016-09-21 20:36:07 +0100

    Add copyright

--- liblogic/src/main/kotlin/com/pacoworks/dereference/core/ui/ControllerBinder.kt
+++ liblogic/src/main/kotlin/com/pacoworks/dereference/core/ui/ControllerBinder.kt
@@ -1,10 +1,20 @@
-package com.pacoworks.dereference.core.ui
+/*
+ * Copyright (c) pakoito 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.jakewharton.rxrelay.BehaviorRelay
-import com.pacoworks.dereference.core.reactive.ConductorLifecycle
-import rx.Observable
-import rx.Scheduler
-import rx.Subscription
+package com.pacoworks.dereference.core.ui
 
 fun <T> bind(lifecycleObservable: rx.Observable<com.pacoworks.dereference.core.reactive.ConductorLifecycle>, mainThreadScheduler: rx.Scheduler, state: com.jakewharton.rxrelay.BehaviorRelay<T>, doView: (T) -> Unit): rx.Subscription =
         lifecycleObservable
