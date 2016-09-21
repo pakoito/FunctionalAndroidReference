271182db19caa21d853b67788c26ff063498fbcb
pakoito <jueces.jus@gmail.com>
2016-09-21 20:08:29 +0100

    Fix sealed class inefficiency

--- liblogic/src/main/kotlin/com/pacoworks/dereference/core/reactive/ActivityResult.kt
+++ liblogic/src/main/kotlin/com/pacoworks/dereference/core/reactive/ActivityResult.kt
@@ -1,8 +1,8 @@
 package com.pacoworks.dereference.core.reactive
 
-sealed class ActivityResult(val requestCode: Int) {
-    data class SuccessWithData(private val reqCode: Int, val data : Map<String, Any>) : ActivityResult(reqCode)
-    data class FailureWithData(private val reqCode: Int, val data : Map<String, Any>) : ActivityResult(reqCode)
-    data class Success(private val reqCode: Int) : ActivityResult(reqCode)
-    data class Failure(private val reqCode: Int) : ActivityResult(reqCode)
+sealed class ActivityResult(open val requestCode: Int) {
+    data class SuccessWithData(override val requestCode: Int, val data : Map<String, Any>) : ActivityResult(requestCode)
+    data class FailureWithData(override val requestCode: Int, val data : Map<String, Any>) : ActivityResult(requestCode)
+    data class Success(override val requestCode: Int) : ActivityResult(requestCode)
+    data class Failure(override val requestCode: Int) : ActivityResult(requestCode)
 }
